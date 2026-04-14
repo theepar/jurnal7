8a208e2edbdc09c99fdd202345bad054bceef121
Wild-Mane <fizlevine@gmail.com>
2026-04-14 08:20:16 +0700

    push ke-3

--- Program.cs
+++ Program.cs
@@ -7,3 +7,6 @@ test.ReadJson();
 
 Watchlist_103022400063 test2 = new Watchlist_103022400063();
 test2.ReadJSON();
+
+GenreDictionary_103022400063 test3 = new GenreDictionary_103022400063();
+test3.ReadJSON();
--- obj/Debug/net10.0/Modul7_Kelompok Radit.AssemblyInfo.cs
+++ obj/Debug/net10.0/Modul7_Kelompok Radit.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Modul7_Kelompok Radit")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e9fbbbe6787482769e1959fec20170ae179feb5c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+741334e058e0795262af2535c0772c8c943ad146")]
 [assembly: System.Reflection.AssemblyProductAttribute("Modul7_Kelompok Radit")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Modul7_Kelompok Radit")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
