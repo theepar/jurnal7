e9fbbbe6787482769e1959fec20170ae179feb5c
Wild-Mane <fizlevine@gmail.com>
2026-04-14 07:37:15 +0700

    Json des1

---
+++ FilmFavorit_.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Modul7_Kelompok_Radit
+{
+    public class filmfavorit_103022400063
+    {
+        [JsonInclude]
+        public string title { get; set; }
+        [JsonInclude]
+        public string director {  get; set; }
+        [JsonInclude]
+        public string genre{  get; set; }
+        [JsonInclude]
+        public int rating{  get; set; }
+        [JsonInclude]
+        public int durationMinutes{  get; set; }
+        [JsonInclude]
+        public bool isWatched{  get; set; }
+
+    }
+    public class FilmFavorit_103022400063
+    {
+        [JsonInclude]
+        public List<filmfavorit_103022400063> filmfavorit {  get; set; }
+        public void ReadJson()
+        {
+            string jsonstring = File.ReadAllText("D:\\jurnal7\\jurnal7_1_103022400063_103022400063.json");
+            filmfavorit_103022400063 film = JsonSerializer.Deserialize<filmfavorit_103022400063>(jsonstring);
+
+            Console.WriteLine("Film");
+
+            Console.WriteLine($"title: {film.title} Director: {film.director} Genre: {film.genre} rating:{film.rating} durationMinutes:{film.durationMinutes} isWatched:{film.isWatched}");
+        }
+ 
+           
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using Modul7_Kelompok_Radit;
+
+
+FilmFavorit_103022400063 test = new FilmFavorit_103022400063();
+test.ReadJson();
