1866186ac086befb7d391cc2390a5dbd4a96e17f
unknown <fadhilra08@gmail.com>
2026-04-14 07:02:20 +0700

    menambahkan class FilmFavorit_103022400023

---
+++ FilmFavorit_103022400023.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json;
+
+namespace Modul7_Kelompok_Radit
+{
+    internal class FilmFavorit_103022400023
+    {
+        public class Film
+        {
+            public string title { get; set; }
+            public string director { get; set; }
+            public string year { get; set; }
+            public string genre { get; set; }
+            public float rating { get; set; }
+            public int durationMinutes { get; set; }
+            public bool isWatched { get; set; }
+        }
+
+        public void ReadJSON()
+        {
+            string baseDirectory = AppDomain.CurrentDomain.BaseDirectory;
+            string projectDirectory = Path.GetFullPath(Path.Combine(baseDirectory, @"..\..\..\"));
+            string filePath = Path.Combine(projectDirectory, "jurnal7_1_103022400023.json");
+
+            if (File.Exists(filePath))
+            {
+                string jsonString = File.ReadAllText(filePath);
+                Film f = JsonSerializer.Deserialize<Film>(jsonString);
+                Console.WriteLine($"{f.title} ({f.year} - {f.rating}), genre: {f.genre}");
+            }
+            else
+            {
+                Console.WriteLine($"error: {filePath} not found");
+            }
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+
+using Modul7_Kelompok_Radit;
+
+FilmFavorit_103022400023 f = new FilmFavorit_103022400023();
+f.ReadJSON();
