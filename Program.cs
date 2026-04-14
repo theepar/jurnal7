7144819ede67912b934d574fb5beffc1a7b974b4
thatFarhan <fabdillahmu@gmail.com>
2026-04-14 07:30:14 +0700

    menambahkan class Watchlist_103022400083.cs

--- Program.cs
+++ Program.cs
@@ -1,3 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 FilmFavorit_103022400083 film = new FilmFavorit_103022400083();
 film.ReadJSON();
+
+Console.WriteLine("");
+
+Watchlist_103022400083 watchlist = new Watchlist_103022400083();
+watchlist.ReadJSON();
---
+++ Watchlist_103022400083.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Text.Json;
+
+public class Watchlist
+{
+	public string watchlistName;
+	public string createdBy;
+	public Movie[] movies;
+}
+
+public class Movie
+{
+	public string id;
+	public string title;
+	public int year;
+	public string genre;
+	public double rating;
+}
+
+public class Watchlist_103022400083
+{
+    public void ReadJSON()
+    {
+        string json = System.IO.File.ReadAllText(@"../../../jurnal7_2_103022400083.json");
+
+        var options = new JsonSerializerOptions
+        {
+            IncludeFields = true
+        };
+
+        Watchlist watchlist = JsonSerializer.Deserialize<Watchlist>(json, options);
+
+        Console.WriteLine($"Watchlist name: {watchlist.watchlistName}");
+        Console.WriteLine($"Created by: {watchlist.createdBy}");
+        Console.WriteLine("Movies:");
+
+        foreach (Movie movie in watchlist.movies)
+        {
+            Console.WriteLine(movie.id + " " + movie.title + " (" + movie.year + " - " + movie.rating + ")");
+        }
+    }
+}
