c006f253c0bfd6bec925f62254510b85e10fbedc
RaditAtalla <radityaderachmadie@gmail.com>
2026-04-14 07:26:46 +0700

    deserialization 1 radit

---
+++ FilmFavorit_103022430007.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json;
+
+namespace Modul7_Kelompok_Radit
+{
+    class Film
+    {
+        public string title;
+        public string director;
+        public string year;
+        public string genre;
+        public double rating;
+        public int durationMinutes;
+        public bool isWatched;
+    }
+
+    class FilmFavorit_103022430007
+    {
+        public void ReadJSON()
+        {
+            var fileJson = System.IO.File.ReadAllText(@"../../../jurnal7_1_103022430007.json");
+            var options = new JsonSerializerOptions
+            {
+                IncludeFields = true,
+            };
+
+            Film film = JsonSerializer.Deserialize<Film>(fileJson, options);
+
+            Console.WriteLine("Judul: " + film.title);
+            Console.WriteLine("Sutradara: " + film.director);
+            Console.WriteLine("Tahun: " + film.year);
+            Console.WriteLine("Genre: " + film.genre);
+            Console.WriteLine("Rating: " + film.rating);
+            Console.WriteLine("Durasi (mnt): " + film.durationMinutes);
+            Console.WriteLine("Sudah Nonton?: " + (film.isWatched ? "Sudah" : "Belum"));
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using Modul7_Kelompok_Radit;
+
+FilmFavorit_103022430007 f = new FilmFavorit_103022430007();
+f.ReadJSON();
